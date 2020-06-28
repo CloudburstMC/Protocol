d95704b01e438132229a5ccdac8e6e8dcf68b57c
bundabrg <brendan@grieve.com.au>
2020-06-28 17:16:59 +0800

    Implement ItemStackRequest Serializer, Packet and Data

--- bedrock/bedrock-v407/src/main/java/com/nukkitx/protocol/bedrock/v407/serializer/ItemStackRequestSerializer_v407.java
+++ bedrock/bedrock-v407/src/main/java/com/nukkitx/protocol/bedrock/v407/serializer/ItemStackRequestSerializer_v407.java
@@ -4,7 +4,7 @@ import com.nukkitx.network.VarInts;
 import com.nukkitx.protocol.bedrock.BedrockPacketHelper;
 import com.nukkitx.protocol.bedrock.BedrockPacketSerializer;
 import com.nukkitx.protocol.bedrock.data.inventory.ItemData;
-import com.nukkitx.protocol.bedrock.data.inventory.ItemStackActionDeprecated;
+import com.nukkitx.protocol.bedrock.data.inventory.ItemStackRequestData;
 import com.nukkitx.protocol.bedrock.data.inventory.StackRequestActionType;
 import com.nukkitx.protocol.bedrock.data.inventory.StackRequestSlotInfoData;
 import com.nukkitx.protocol.bedrock.data.inventory.stackrequestactions.AutoCraftRecipeStackRequestActionData;
@@ -22,6 +22,7 @@ import com.nukkitx.protocol.bedrock.data.inventory.stackrequestactions.PlaceStac
 import com.nukkitx.protocol.bedrock.data.inventory.stackrequestactions.StackRequestActionData;
 import com.nukkitx.protocol.bedrock.data.inventory.stackrequestactions.SwapStackRequestActionData;
 import com.nukkitx.protocol.bedrock.data.inventory.stackrequestactions.TakeStackRequestActionData;
+import com.nukkitx.protocol.bedrock.data.inventory.stackrequestactions.TransferStackRequestActionData;
 import com.nukkitx.protocol.bedrock.packet.ItemStackRequestPacket;
 import io.netty.buffer.ByteBuf;
 import lombok.AccessLevel;
@@ -41,62 +42,48 @@ public class ItemStackRequestSerializer_v407 implements BedrockPacketSerializer<
             VarInts.writeInt(buf, requests.getRequestId());
 
             helper.writeArray(buf, requests.getActions(), (byteBuf, action) -> {
-                switch (action.getType()) {
-                    case 0:
-                    case 1:
-                    case 2:
-                        byteBuf.writeByte(action.getBaseByte0());
-                        byteBuf.writeByte(action.getBaseByte1());
-                        byteBuf.writeByte(action.getBaseByte2());
-                        VarInts.writeInt(byteBuf, action.getVarInt0());
-                        byteBuf.writeByte(action.getFlagsByte0());
-                        byteBuf.writeByte(action.getFlagsByte1());
-                        VarInts.writeInt(byteBuf, action.getFlagsVarInt0());
+                StackRequestActionType type = StackRequestActionType.byClass(action.getClass());
+                byteBuf.writeByte(type.ordinal());
+
+                switch (type) {
+                    case TAKE:
+                    case PLACE:
+                    case SWAP:
+                        byteBuf.writeByte(((TransferStackRequestActionData) action).getCount());
+                        writeStackRequestSlotInfo(byteBuf, helper, ((TransferStackRequestActionData) action).getSource());
+                        writeStackRequestSlotInfo(byteBuf, helper, ((TransferStackRequestActionData) action).getDestination());
                         break;
-                    case 3:
-                        byteBuf.writeByte(action.getBaseByte0());
-                        byteBuf.writeByte(action.getBaseByte1());
-                        byteBuf.writeByte(action.getBaseByte2());
-                        VarInts.writeInt(byteBuf, action.getVarInt0());
-                        byteBuf.writeBoolean(action.isBool0());
+                    case DROP:
+                        byteBuf.writeByte(((DropStackRequestActionData) action).getCount());
+                        writeStackRequestSlotInfo(byteBuf, helper, ((DropStackRequestActionData) action).getSource());
+                        byteBuf.writeBoolean(((DropStackRequestActionData) action).isRandomly());
                         break;
-                    case 4:
-                    case 5:
-                        byteBuf.writeByte(action.getBaseByte0());
-                        byteBuf.writeByte(action.getBaseByte1());
-                        byteBuf.writeByte(action.getBaseByte2());
-                        VarInts.writeInt(byteBuf, action.getVarInt0());
+                    case DESTROY:
+                    case CONSUME:
+                        byteBuf.writeByte(((DestroyStackRequestActionData) action).getCount());
+                        writeStackRequestSlotInfo(byteBuf, helper, ((DestroyStackRequestActionData) action).getSource());
                         break;
-                    case 6:
-                        byteBuf.writeByte(action.getByte0());
+                    case CREATE:
+                        byteBuf.writeByte(((CreateStackRequestActionData) action).getSlot());
                         break;
-                    case 8:
-                        VarInts.writeInt(byteBuf, action.getVarInt0());
-                        VarInts.writeInt(byteBuf, action.getVarInt1());
+                    case BEACON_PAYMENT:
+                        VarInts.writeInt(byteBuf, ((BeaconPaymentStackRequestActionData) action).getPrimaryEffect());
+                        VarInts.writeInt(byteBuf, ((BeaconPaymentStackRequestActionData) action).getSecondaryEffect());
                         break;
-                    case 9:
-                    case 10:
-                    case 11:
-                    case 12:
-                    case 13:
-                    case 14:
-                    case 15:
-                        VarInts.writeInt(byteBuf, action.getVarInt0());
+                    case CRAFT_RECIPE:
+                    case CRAFT_RECIPE_AUTO:
+                        VarInts.writeInt(byteBuf, ((CraftRecipeStackRequestActionData) action).getRecipeNetworkId());
                         break;
-                    case 17:
-                        helper.writeArray(byteBuf, action.getItems(), helper::writeItem);
+                    case CRAFT_CREATIVE:
+                        VarInts.writeInt(byteBuf, ((CraftCreativeStackRequestActionData) action).getCreativeItemNetworkId());
+                        break;
+                    case CRAFT_RESULTS_DEPRECATED:
+                        helper.writeArray(byteBuf, ((CraftResultsDeprecatedStackRequestActionData) action).getResultItems(), helper::writeItem);
+                        byteBuf.writeByte(((CraftResultsDeprecatedStackRequestActionData) action).getTimesCrafted());
                         break;
                 }
             });
         });
-    }
-
-    public StackRequestSlotInfoData readStackRequestSlotInfo(ByteBuf buffer, BedrockPacketHelper helper) {
-        return new StackRequestSlotInfoData(
-                buffer.readByte(),
-                buffer.readByte(),
-                VarInts.readInt(buffer)
-        );
     }
 
     @Override
@@ -175,72 +162,22 @@ public class ItemStackRequestSerializer_v407 implements BedrockPacketSerializer<
 
                 // Fallback
                 return new CraftNonImplementedStackRequestActionData();
-
-
-//                boolean bool0 = false;
-//                byte byte0 = 0;
-//                int varInt0 = 0;
-//                int varInt1 = 0;
-//                byte baseByte0 = 0;
-//                byte baseByte1 = 0;
-//                byte baseByte2 = 0;
-//                int baseVarInt0 = 0;
-//                byte flagsByte0 = 0;
-//                byte flagsByte1 = 0;
-//                int flagsVarInt0 = 0;
-//                List<ItemData> items = null;
-//
-//                switch (type) {
-//                    case 0:
-//                    case 1:
-//                    case 2:
-//                        baseByte0 = byteBuf.readByte();
-//                        baseByte1 = byteBuf.readByte();
-//                        baseByte2 = byteBuf.readByte();
-//                        baseVarInt0 = VarInts.readInt(byteBuf);
-//                        flagsByte0 = byteBuf.readByte();
-//                        flagsByte1 = byteBuf.readByte();
-//                        flagsVarInt0 = VarInts.readInt(byteBuf);
-//                        break;
-//                    case 3:
-//                        baseByte0 = byteBuf.readByte();
-//                        baseByte1 = byteBuf.readByte();
-//                        baseByte2 = byteBuf.readByte();
-//                        baseVarInt0 = VarInts.readInt(byteBuf);
-//                        bool0 = byteBuf.readBoolean();
-//                        break;
-//                    case 4:
-//                    case 5:
-//                        baseByte0 = byteBuf.readByte();
-//                        baseByte1 = byteBuf.readByte();
-//                        baseByte2 = byteBuf.readByte();
-//                        baseVarInt0 = VarInts.readInt(byteBuf);
-//                        break;
-//                    case 6:
-//                        byte0 = byteBuf.readByte();
-//                        break;
-//                    case 8:
-//                        varInt0 = VarInts.readInt(byteBuf);
-//                        varInt1 = VarInts.readInt(byteBuf);
-//                        break;
-//                    case 9:
-//                    case 10:
-//                    case 11:
-//                    case 12:
-//                    case 13:
-//                    case 14:
-//                    case 15:
-//                        varInt0 = VarInts.readUnsignedInt(byteBuf);
-//                        break;
-//                    case 17:
-//                        items = new ArrayList<>();
-//                        helper.readArray(byteBuf, items, helper::readItem);
-//                        break;
-//                }
-//                return new ItemStackActionDeprecated(type, bool0, byte0, varInt0, varInt1, baseByte0, baseByte1, baseByte2,
-//                        baseVarInt0, flagsByte0, flagsByte1, flagsVarInt0, items);
             });
-            return new ItemStackRequestPacket.Request(requestId, actions);
+            return new ItemStackRequestData(requestId, actions.toArray(new StackRequestActionData[0]));
         });
+    }
+
+    public StackRequestSlotInfoData readStackRequestSlotInfo(ByteBuf buffer, BedrockPacketHelper helper) {
+        return new StackRequestSlotInfoData(
+                buffer.readByte(),
+                buffer.readByte(),
+                VarInts.readInt(buffer)
+        );
+    }
+
+    public void writeStackRequestSlotInfo(ByteBuf buffer, BedrockPacketHelper helper, StackRequestSlotInfoData data) {
+        buffer.writeByte(data.getContainerId());
+        buffer.writeByte(data.getSlot());
+        VarInts.writeInt(buffer, data.getStackNetworkId());
     }
 }
