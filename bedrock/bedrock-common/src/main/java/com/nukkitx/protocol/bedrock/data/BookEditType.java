535cde0329d55bed46c682041a213c5a6d336ceb
D3ATHBRINGER13 <53559772+D3ATHBRINGER13@users.noreply.github.com>
2020-06-29 23:43:44 +0100

    Alphabetise BookEditType

--- bedrock/bedrock-common/src/main/java/com/nukkitx/protocol/bedrock/data/BookEditType.java
+++ bedrock/bedrock-common/src/main/java/com/nukkitx/protocol/bedrock/data/BookEditType.java
@@ -1,9 +1,9 @@
 package com.nukkitx.protocol.bedrock.data;
 
 public enum BookEditType {
-    REPLACE_PAGE,
     ADD_PAGE,
     DELETE_PAGE,
-    SWAP_PAGES,
-    SIGN_BOOK
+    REPLACE_PAGE,
+    SIGN_BOOK,
+    SWAP_PAGES
 }
