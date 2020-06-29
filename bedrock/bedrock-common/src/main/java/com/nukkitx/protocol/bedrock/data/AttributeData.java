1a07c08c395e1760e8f97bc325122c434778a778
D3ATHBRINGER13 <53559772+D3ATHBRINGER13@users.noreply.github.com>
2020-06-29 23:37:37 +0100

    Alphabetise AttributeData

--- bedrock/bedrock-common/src/main/java/com/nukkitx/protocol/bedrock/data/AttributeData.java
+++ bedrock/bedrock-common/src/main/java/com/nukkitx/protocol/bedrock/data/AttributeData.java
@@ -6,17 +6,17 @@ import lombok.Value;
 @Value
 @AllArgsConstructor
 public class AttributeData {
-    private final String name;
-    private final float minimum;
-    private final float maximum;
-    private final float value;
     private final float defaultValue;
+    private final float maximum;
+    private final float minimum;
+    private final float value;
+    private final String name ;
 
     public AttributeData(String name, float minimum, float maximum, float value) {
-        this.name = name;
-        this.minimum = minimum;
-        this.maximum = maximum;
-        this.value = value;
         this.defaultValue = maximum;
+        this.maximum = maximum;
+        this.minimum = minimum;
+        this.name = name;
+        this.value = value;
     }
 }
