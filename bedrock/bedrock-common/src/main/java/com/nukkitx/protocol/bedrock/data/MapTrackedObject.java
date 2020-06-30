d3153a1b3312122e05848f2a942fb98d7ccaa1f1
D3ATHBRINGER13 <53559772+D3ATHBRINGER13@users.noreply.github.com>
2020-06-30 19:22:31 +0100

    MapTrackedObject

--- bedrock/bedrock-common/src/main/java/com/nukkitx/protocol/bedrock/data/MapTrackedObject.java
+++ bedrock/bedrock-common/src/main/java/com/nukkitx/protocol/bedrock/data/MapTrackedObject.java
@@ -10,19 +10,19 @@ public class MapTrackedObject {
     private final Type type;
     private long entityId;
     private Vector3i position;
-
+    
+    public MapTrackedObject(Vector3i position) {
+        this.type = Type.BLOCK;
+        this.position = position;
+    }
+    
     public MapTrackedObject(long entityId) {
         this.type = Type.ENTITY;
         this.entityId = entityId;
     }
 
-    public MapTrackedObject(Vector3i position) {
-        this.type = Type.BLOCK;
-        this.position = position;
-    }
-
     public enum Type {
-        ENTITY,
-        BLOCK
+        BLOCK,
+        ENTITY
     }
 }
