ce8d99fdba9446f27bde9a0280a9423bc32dfaa7
D3ATHBRINGER13 <53559772+D3ATHBRINGER13@users.noreply.github.com>
2020-06-30 19:23:55 +0100

    NPCRequestType

--- bedrock/bedrock-common/src/main/java/com/nukkitx/protocol/bedrock/data/NPCRequestType.java
+++ bedrock/bedrock-common/src/main/java/com/nukkitx/protocol/bedrock/data/NPCRequestType.java
@@ -1,7 +1,7 @@
 package com.nukkitx.protocol.bedrock.data;
 
 public enum NpcRequestType {
-    SET_ACTION,
+    EXECUTE_CLOSING_COMMANDS,
     EXECUTE_COMMAND_ACTION,
-    EXECUTE_CLOSING_COMMANDS
+    SET_ACTION
 }
