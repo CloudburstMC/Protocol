8de9507eddb95825dc7ba92fb00650adb54b72b1
D3ATHBRINGER13 <53559772+D3ATHBRINGER13@users.noreply.github.com>
2020-06-30 19:20:36 +0100

    MapDecoration

--- bedrock/bedrock-common/src/main/java/com/nukkitx/protocol/bedrock/data/MapDecoration.java
+++ bedrock/bedrock-common/src/main/java/com/nukkitx/protocol/bedrock/data/MapDecoration.java
@@ -4,10 +4,10 @@ import lombok.Value;
 
 @Value
 public class MapDecoration {
+    private final int color;
     private final int image;
     private final int rotation;
     private final int xOffset;
     private final int yOffset;
     private final String label;
-    private final int color;
 }
