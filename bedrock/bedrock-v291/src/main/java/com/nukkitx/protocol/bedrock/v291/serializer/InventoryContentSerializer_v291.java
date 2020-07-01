3f720ca77705a83a2855a6d139bfa35789b7aa1e
EmreTr1 <emre.yavuz.842@gmail.com>
2020-07-01 14:20:21 +0300

    simplify some codes Removed InventoryContentSerializer_v407

--- bedrock/bedrock-v291/src/main/java/com/nukkitx/protocol/bedrock/v291/serializer/InventoryContentSerializer_v291.java
+++ bedrock/bedrock-v291/src/main/java/com/nukkitx/protocol/bedrock/v291/serializer/InventoryContentSerializer_v291.java
@@ -13,24 +13,15 @@ import lombok.NoArgsConstructor;
 public class InventoryContentSerializer_v291 implements BedrockPacketSerializer<InventoryContentPacket> {
     public static final InventoryContentSerializer_v291 INSTANCE = new InventoryContentSerializer_v291();
 
-
     @Override
     public void serialize(ByteBuf buffer, BedrockPacketHelper helper, InventoryContentPacket packet) {
         VarInts.writeUnsignedInt(buffer, packet.getContainerId());
-
-        VarInts.writeUnsignedInt(buffer, packet.getItems().size());
-        for (ItemInstance itemInstance : packet.getItems()) {
-            helper.writeItemInstance(buffer, itemInstance);
-        }
+        helper.writeArray(buffer, packet.getItems(), helper::writeItemInstance);
     }
 
     @Override
     public void deserialize(ByteBuf buffer, BedrockPacketHelper helper, InventoryContentPacket packet) {
         packet.setContainerId(VarInts.readUnsignedInt(buffer));
-
-        int count = VarInts.readUnsignedInt(buffer);
-        for (int i = 0; i < count; i++) {
-            packet.getItems().add(helper.readItemInstance(buffer));
-        }
+        helper.readArray(buffer, packet.getItems(), helper::readItemInstance);
     }
 }
