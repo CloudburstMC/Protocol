2b266a3720343ad573ca77b2b80da50c4d579fcb
bundabrg <brendan@grieve.com.au>
2020-08-24 17:30:27 +0800

    Add v411 Protocol

---
+++ bedrock/bedrock-v411/src/main/java/com/nukkitx/protocol/bedrock/v411/BedrockPacketHelper_v411.java
@@ -0,0 +1,12 @@
+package com.nukkitx.protocol.bedrock.v411;
+
+import com.nukkitx.protocol.bedrock.v409.BedrockPacketHelper_v409;
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+
+
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class BedrockPacketHelper_v411 extends BedrockPacketHelper_v409 {
+    public static final BedrockPacketHelper_v411 INSTANCE = new BedrockPacketHelper_v411();
+
+}
