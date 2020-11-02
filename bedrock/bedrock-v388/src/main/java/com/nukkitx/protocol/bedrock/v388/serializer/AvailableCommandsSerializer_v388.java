15e8fd9fe147798cbd662934014800a3fcc2b95b
Alemiz112 <alex@mizerak.eu>
2020-11-02 21:59:59 +0100

    Servers must include at least constraints count

--- bedrock/bedrock-v388/src/main/java/com/nukkitx/protocol/bedrock/v388/serializer/AvailableCommandsSerializer_v388.java
+++ bedrock/bedrock-v388/src/main/java/com/nukkitx/protocol/bedrock/v388/serializer/AvailableCommandsSerializer_v388.java
@@ -158,9 +158,7 @@ public class AvailableCommandsSerializer_v388 extends AvailableCommandsSerialize
                     flagList, command.getPermission(), aliases, overloads));
         }
 
-        // Read constraints if included
-        if (buffer.readableBytes() > 1){
-            helper.readArray(buffer, packet.getConstraints(), buf -> helper.readCommandEnumConstraints(buf, enums, enumValues));
-        }
+        // Constraints
+        helper.readArray(buffer, packet.getConstraints(), buf -> helper.readCommandEnumConstraints(buf, enums, enumValues));
     }
 }
