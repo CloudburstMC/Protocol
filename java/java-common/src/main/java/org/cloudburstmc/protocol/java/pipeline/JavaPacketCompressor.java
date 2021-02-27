7c062a1d4beadee3ab945739345c0c1c54016a33
SupremeMortal <6178101+SupremeMortal@users.noreply.github.com>
2021-02-27 12:46:22 +0000

    Optimise compression

--- java/java-common/src/main/java/org/cloudburstmc/protocol/java/pipeline/JavaPacketCompressor.java
+++ java/java-common/src/main/java/org/cloudburstmc/protocol/java/pipeline/JavaPacketCompressor.java
@@ -1,28 +1,19 @@
 package org.cloudburstmc.protocol.java.pipeline;
 
-import com.nukkitx.natives.util.Natives;
-import com.nukkitx.natives.zlib.Deflater;
 import com.nukkitx.network.VarInts;
+import com.nukkitx.protocol.util.Zlib;
 import io.netty.buffer.ByteBuf;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.handler.codec.MessageToByteEncoder;
 import lombok.AllArgsConstructor;
 import org.cloudburstmc.protocol.java.JavaSession;
 
-import java.nio.ByteBuffer;
+import java.util.zip.Deflater;
 
 @AllArgsConstructor
 public class JavaPacketCompressor extends MessageToByteEncoder<ByteBuf> {
-    private static final ThreadLocal<Deflater> DEFLATER = new ThreadLocal<Deflater>(){
-        @Override
-        protected Deflater initialValue() {
-            return Natives.ZLIB.get().create(-1, false);
-        }
-    };
 
     private final JavaSession session;
-
-    private final ByteBuffer buffer = ByteBuffer.allocate(8192);
 
     @Override
     protected void encode(ChannelHandlerContext ctx, ByteBuf in, ByteBuf out) {
@@ -34,12 +25,6 @@ public class JavaPacketCompressor extends MessageToByteEncoder<ByteBuf> {
         }
 
         VarInts.writeUnsignedInt(out, in.readableBytes());
-        Deflater deflater = DEFLATER.get();
-        deflater.setInput(in.internalNioBuffer(in.readerIndex(), in.readableBytes()));
-        while (!deflater.finished()) {
-            int size = deflater.deflate(this.buffer);
-            out.writeBytes(this.buffer.array(), 0, size);
-        }
-        deflater.reset();
+        Zlib.DEFAULT.deflate(in, out, Deflater.DEFAULT_COMPRESSION);
     }
 }
