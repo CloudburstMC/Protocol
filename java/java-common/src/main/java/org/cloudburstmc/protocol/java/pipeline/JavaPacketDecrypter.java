47254c81088b848a4cf6db51e47a5425adbd8ee4
SupremeMortal <6178101+SupremeMortal@users.noreply.github.com>
2021-02-27 16:13:11 +0000

    Use internal ByteBuffers for encryption
    No idea if using the same buffer is going to work but it's worth a shot to save some memory allocations.

--- java/java-common/src/main/java/org/cloudburstmc/protocol/java/pipeline/JavaPacketDecrypter.java
+++ java/java-common/src/main/java/org/cloudburstmc/protocol/java/pipeline/JavaPacketDecrypter.java
@@ -8,32 +8,17 @@ import lombok.RequiredArgsConstructor;
 import javax.crypto.Cipher;
 import java.util.List;
 
-// TODO: Native encryption
 @RequiredArgsConstructor
 public class JavaPacketDecrypter extends MessageToMessageDecoder<ByteBuf> {
     private final Cipher cipher;
 
-    private byte[] heap = new byte[0];
-
     @Override
-    protected void decode(ChannelHandlerContext ctx, ByteBuf in, List<Object> out) {
-        try {
-            int length = in.readableBytes();
-            byte[] bytes = toByteArray(in);
-            ByteBuf heapBuffer = ctx.alloc().heapBuffer(this.cipher.getOutputSize(length));
-            heapBuffer.writerIndex(this.cipher.update(bytes, 0, length, heapBuffer.array(), heapBuffer.arrayOffset()));
-            out.add(heapBuffer);
-        } catch (Throwable ex) {
-            ex.printStackTrace();
-        }
-    }
+    protected void decode(ChannelHandlerContext ctx, ByteBuf in, List<Object> out) throws Exception {
+        // TODO: Native encryption?
+        // Encryption is copy safe so we can use the same buffer.
+        this.cipher.update(in.internalNioBuffer(in.readerIndex(), in.readableBytes()),
+                in.internalNioBuffer(in.readerIndex(), in.readableBytes()));
 
-    private byte[] toByteArray(ByteBuf buffer) {
-        int length = buffer.readableBytes();
-        if (this.heap.length < length) {
-            this.heap = new byte[length];
-        }
-        buffer.readBytes(this.heap, 0, length);
-        return this.heap;
+        out.add(in);
     }
 }
--- java/java-common/src/main/java/org/cloudburstmc/protocol/java/pipeline/JavaPacketEncrypter.java
+++ java/java-common/src/main/java/org/cloudburstmc/protocol/java/pipeline/JavaPacketEncrypter.java
@@ -2,39 +2,23 @@ package org.cloudburstmc.protocol.java.pipeline;
 
 import io.netty.buffer.ByteBuf;
 import io.netty.channel.ChannelHandlerContext;
-import io.netty.handler.codec.MessageToByteEncoder;
+import io.netty.handler.codec.MessageToMessageEncoder;
 import lombok.RequiredArgsConstructor;
 
 import javax.crypto.Cipher;
+import java.util.List;
 
-// TODO: Native encryption
 @RequiredArgsConstructor
-public class JavaPacketEncrypter extends MessageToByteEncoder<ByteBuf> {
+public class JavaPacketEncrypter extends MessageToMessageEncoder<ByteBuf> {
     private final Cipher cipher;
 
-    private byte[] heap = new byte[0];
-
     @Override
-    protected void encode(ChannelHandlerContext ctx, ByteBuf in, ByteBuf out) {
-        try {
-            int length = in.readableBytes();
-            byte[] bytes = toByteArray(in);
-            int outputSize = this.cipher.getOutputSize(length);
-            if (this.heap.length < outputSize) {
-                this.heap = new byte[outputSize];
-            }
-            out.writeBytes(this.heap, 0, this.cipher.update(bytes, 0, length, this.heap));
-        } catch (Throwable ex) {
-            ex.printStackTrace();
-        }
-    }
+    protected void encode(ChannelHandlerContext ctx, ByteBuf in, List<Object> out) throws Exception {
+        // TODO: Native encryption?
+        // Encryption is copy safe so we can use the same buffer.
+        this.cipher.update(in.internalNioBuffer(in.readerIndex(), in.readableBytes()),
+                in.internalNioBuffer(in.readerIndex(), in.readableBytes()));
 
-    private byte[] toByteArray(ByteBuf buffer) {
-        int length = buffer.readableBytes();
-        if (this.heap.length < length) {
-            this.heap = new byte[length];
-        }
-        buffer.readBytes(this.heap, 0, length);
-        return this.heap;
+        out.add(in);
     }
 }
