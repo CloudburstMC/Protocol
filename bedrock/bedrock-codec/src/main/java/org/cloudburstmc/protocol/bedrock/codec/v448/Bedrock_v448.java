f6a8b855f21a56bcb43076379cd777d6f5bddcde
SupremeMortal <6178101+SupremeMortal@users.noreply.github.com>
2021-08-12 16:31:30 +0100

    Register 448 codec

--- bedrock/bedrock-codec/src/main/java/org/cloudburstmc/protocol/bedrock/codec/v448/Bedrock_v448.java
+++ bedrock/bedrock-codec/src/main/java/org/cloudburstmc/protocol/bedrock/codec/v448/Bedrock_v448.java
@@ -2,11 +2,24 @@ package org.cloudburstmc.protocol.bedrock.codec.v448;
 
 import lombok.AccessLevel;
 import lombok.NoArgsConstructor;
+import org.cloudburstmc.protocol.bedrock.codec.BedrockCodec;
+import org.cloudburstmc.protocol.bedrock.codec.v291.serializer.LevelEventSerializer_v291;
+import org.cloudburstmc.protocol.bedrock.codec.v291.serializer.LevelSoundEvent1Serializer_v291;
+import org.cloudburstmc.protocol.bedrock.codec.v313.serializer.LevelSoundEvent2Serializer_v313;
+import org.cloudburstmc.protocol.bedrock.codec.v332.serializer.LevelSoundEventSerializer_v332;
+import org.cloudburstmc.protocol.bedrock.codec.v422.Bedrock_v422;
+import org.cloudburstmc.protocol.bedrock.codec.v440.BedrockCodecHelper_v440;
 import org.cloudburstmc.protocol.bedrock.codec.v440.Bedrock_v440;
+import org.cloudburstmc.protocol.bedrock.codec.v440.serializer.AddVolumeEntitySerializer_v440;
+import org.cloudburstmc.protocol.bedrock.codec.v440.serializer.RemoveVolumeEntitySerializer_v440;
+import org.cloudburstmc.protocol.bedrock.codec.v440.serializer.StartGameSerializer_v440;
+import org.cloudburstmc.protocol.bedrock.codec.v440.serializer.SyncEntityPropertySerializer_v440;
+import org.cloudburstmc.protocol.bedrock.codec.v448.serializer.*;
 import org.cloudburstmc.protocol.bedrock.data.LevelEventType;
 import org.cloudburstmc.protocol.bedrock.data.ParticleType;
 import org.cloudburstmc.protocol.bedrock.data.SoundEvent;
 import org.cloudburstmc.protocol.bedrock.data.entity.EntityFlag;
+import org.cloudburstmc.protocol.bedrock.packet.*;
 import org.cloudburstmc.protocol.common.util.TypeMap;
 
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
@@ -27,5 +40,21 @@ public class Bedrock_v448 extends Bedrock_v440 {
             .insert(361, SoundEvent.EXTINGUISH_CANDLE)
             .insert(362, SoundEvent.AMBIENT_CANDLE)
             .insert(363, SoundEvent.UNDEFINED)
+            .build();
+
+    public static final BedrockCodec CODEC = Bedrock_v440.CODEC.toBuilder()
+            .protocolVersion(448)
+            .minecraftVersion("1.17.10")
+            .helper(() -> new BedrockCodecHelper_v440(ENTITY_DATA, ENTITY_DATA_TYPES, ENTITY_FLAGS, GAME_RULE_TYPES, ITEM_STACK_REQUEST_TYPES))
+            .updateSerializer(AvailableCommandsPacket.class, new AvailableCommandsSerializer_v448(COMMAND_PARAMS))
+            .updateSerializer(LevelSoundEvent1Packet.class, new LevelSoundEvent1Serializer_v291(SOUND_EVENTS))
+            .updateSerializer(LevelSoundEvent2Packet.class, new LevelSoundEvent2Serializer_v313(SOUND_EVENTS))
+            .updateSerializer(LevelSoundEventPacket.class, new LevelSoundEventSerializer_v332(SOUND_EVENTS))
+            .updateSerializer(LevelEventPacket.class, new LevelEventSerializer_v291(LEVEL_EVENTS))
+            .updateSerializer(NpcRequestPacket.class, NpcRequestSerializer_v448.INSTANCE)
+            .updateSerializer(SetTitlePacket.class, SetTitleSerializer_v448.INSTANCE)
+            .updateSerializer(ResourcePacksInfoPacket.class, ResourcePacksInfoSerializer_v448.INSTANCE)
+            .registerPacket(SimulationTypePacket.class, SimulationTypeSerializer_v448.INSTANCE, 168)
+            .registerPacket(NpcDialoguePacket.class, NpcDialogueSerializer_v448.INSTANCE, 169)
             .build();
 }
