151295f97cbf6c14bbab7fb585e0fa84f82e5665
Alemiz112 <alex@mizerak.eu>
2021-09-06 23:02:13 +0200

    Fix blobs order in ClientCacheBlobStatusPacket

--- bedrock/bedrock-v361/src/main/java/com/nukkitx/protocol/bedrock/v361/serializer/ClientCacheBlobStatusSerializer_v361.java
+++ bedrock/bedrock-v361/src/main/java/com/nukkitx/protocol/bedrock/v361/serializer/ClientCacheBlobStatusSerializer_v361.java
@@ -17,28 +17,28 @@ public class ClientCacheBlobStatusSerializer_v361 implements BedrockPacketSerial
 
     @Override
     public void serialize(ByteBuf buffer, BedrockPacketHelper helper, ClientCacheBlobStatusPacket packet) {
-        LongList acks = packet.getAcks();
         LongList nacks = packet.getNaks();
-        VarInts.writeUnsignedInt(buffer, acks.size());
+        LongList acks = packet.getAcks();
         VarInts.writeUnsignedInt(buffer, nacks.size());
+        VarInts.writeUnsignedInt(buffer, acks.size());
 
-        acks.forEach((LongConsumer) buffer::writeLongLE);
         nacks.forEach((LongConsumer) buffer::writeLongLE);
+        acks.forEach((LongConsumer) buffer::writeLongLE);
     }
 
     @Override
     public void deserialize(ByteBuf buffer, BedrockPacketHelper helper, ClientCacheBlobStatusPacket packet) {
-        int acksLength = VarInts.readUnsignedInt(buffer);
         int naksLength = VarInts.readUnsignedInt(buffer);
-
-        LongList acks = packet.getAcks();
-        for (int i = 0; i < acksLength; i++) {
-            acks.add(buffer.readLongLE());
-        }
+        int acksLength = VarInts.readUnsignedInt(buffer);
 
         LongList naks = packet.getNaks();
         for (int i = 0; i < naksLength; i++) {
             naks.add(buffer.readLongLE());
+        }
+
+        LongList acks = packet.getAcks();
+        for (int i = 0; i < acksLength; i++) {
+            acks.add(buffer.readLongLE());
         }
     }
 }
