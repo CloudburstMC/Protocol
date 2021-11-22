fe32e6b988a09cea030e7578d90db34e6730ed77
SupremeMortal <6178101+SupremeMortal@users.noreply.github.com>
2021-11-22 10:36:12 +0000

    1.18.0 fixes

--- bedrock/bedrock-codec/src/main/java/org/cloudburstmc/protocol/bedrock/codec/v475/Bedrock_v475.java
+++ bedrock/bedrock-codec/src/main/java/org/cloudburstmc/protocol/bedrock/codec/v475/Bedrock_v475.java
@@ -5,6 +5,7 @@ import org.cloudburstmc.protocol.bedrock.codec.v291.serializer.LevelEventSeriali
 import org.cloudburstmc.protocol.bedrock.codec.v291.serializer.LevelSoundEvent1Serializer_v291;
 import org.cloudburstmc.protocol.bedrock.codec.v313.serializer.LevelSoundEvent2Serializer_v313;
 import org.cloudburstmc.protocol.bedrock.codec.v332.serializer.LevelSoundEventSerializer_v332;
+import org.cloudburstmc.protocol.bedrock.codec.v465.BedrockCodecHelper_v465;
 import org.cloudburstmc.protocol.bedrock.codec.v471.Bedrock_v471;
 import org.cloudburstmc.protocol.bedrock.codec.v475.serializer.StartGameSerializer_v475;
 import org.cloudburstmc.protocol.bedrock.codec.v475.serializer.SubChunkSerializer_v475;
@@ -21,14 +22,14 @@ public class Bedrock_v475 extends Bedrock_v471 {
             .build();
 
     protected static final TypeMap<SoundEvent> SOUND_EVENTS = Bedrock_v471.SOUND_EVENTS.toBuilder()
-            .replace(371, SoundEvent.RECORD_OTHER_SIDE)
+            .replace(371, SoundEvent.RECORD_OTHERSIDE)
             .insert(372, SoundEvent.UNDEFINED)
             .build();
 
     public static final BedrockCodec CODEC = Bedrock_v471.CODEC.toBuilder()
             .protocolVersion(475)
             .minecraftVersion("1.18.0")
-            .helper(() -> new BedrockCodecHelper_v475(ENTITY_DATA, ENTITY_DATA_TYPES, ENTITY_FLAGS, GAME_RULE_TYPES, ITEM_STACK_REQUEST_TYPES))
+            .helper(() -> new BedrockCodecHelper_v465(ENTITY_DATA, ENTITY_DATA_TYPES, ENTITY_FLAGS, GAME_RULE_TYPES, ITEM_STACK_REQUEST_TYPES))
             .updateSerializer(StartGamePacket.class, StartGameSerializer_v475.INSTANCE)
             .updateSerializer(LevelEventPacket.class, new LevelEventSerializer_v291(LEVEL_EVENTS))
             .updateSerializer(LevelSoundEvent1Packet.class, new LevelSoundEvent1Serializer_v291(SOUND_EVENTS))
