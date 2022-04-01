36561ccf0a1116a68312c10457921bd540a781e3
Alemiz <alex@mizerak.eu>
2022-04-01 22:57:37 +0200

    Add missing methods of SerializedSkin builder

--- bedrock/bedrock-codec/src/main/java/org/cloudburstmc/protocol/bedrock/data/skin/SerializedSkin.java
+++ bedrock/bedrock-codec/src/main/java/org/cloudburstmc/protocol/bedrock/data/skin/SerializedSkin.java
@@ -181,6 +181,8 @@ public class SerializedSkin {
         private String skinColor;
         private List<PersonaPieceData> personaPieces;
         private List<PersonaPieceTintData> tintColors;
+        private String geometryDataEngineVersion;
+        private boolean primaryUser;
 
         Builder() {
         }
@@ -275,6 +277,16 @@ public class SerializedSkin {
             return this;
         }
 
+        public Builder geometryDataEngineVersion(String version) {
+            this.geometryDataEngineVersion = version;
+            return this;
+        }
+
+        public Builder primaryUser(boolean primaryUser) {
+            this.primaryUser = primaryUser;
+            return this;
+        }
+
         public SerializedSkin build() {
             if (playFabId == null) playFabId = "";
             if (animationData == null) animationData = "";
@@ -282,6 +294,7 @@ public class SerializedSkin {
             if (capeId == null) capeId = "";
             if (fullSkinId == null) fullSkinId = skinId + capeId;
             if (armSize == null) armSize = "wide";
+            if (geometryDataEngineVersion == null) geometryDataEngineVersion = "";
             if (skinColor == null) skinColor = "#0";
             if (personaPieces == null) personaPieces = Collections.emptyList();
             if (tintColors == null) tintColors = Collections.emptyList();
@@ -290,8 +303,8 @@ public class SerializedSkin {
                 return SerializedSkin.of(skinId, playFabId, geometryName, skinData, animations, capeData, geometryData,
                         animationData, premium, persona, capeOnClassic, capeId, fullSkinId);
             } else {
-                return SerializedSkin.of(skinId, playFabId, skinResourcePatch, skinData, animations, capeData, geometryData,
-                        animationData, premium, persona, capeOnClassic, capeId, fullSkinId);
+                return SerializedSkin.of(skinId, playFabId, skinResourcePatch, skinData, animations, capeData, geometryData, geometryDataEngineVersion,
+                        animationData, premium, persona, capeOnClassic, primaryUser, capeId, fullSkinId, armSize, skinColor, personaPieces, tintColors);
             }
         }
 
