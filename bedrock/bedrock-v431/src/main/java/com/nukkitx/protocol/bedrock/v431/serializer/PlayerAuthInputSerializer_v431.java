47f92068b206bfaa7114862457c97dc478c5bbb0
Alemiz <alex@mizerak.eu>
2022-04-02 16:35:52 +0200

    PlayerActionType.STOP_BREAK has no data

--- bedrock/bedrock-v431/src/main/java/com/nukkitx/protocol/bedrock/v431/serializer/PlayerAuthInputSerializer_v431.java
+++ bedrock/bedrock-v431/src/main/java/com/nukkitx/protocol/bedrock/v431/serializer/PlayerAuthInputSerializer_v431.java
@@ -1,8 +1,6 @@
 package com.nukkitx.protocol.bedrock.v431.serializer;
 
-import com.nukkitx.network.VarInts;
 import com.nukkitx.protocol.bedrock.BedrockPacketHelper;
-import com.nukkitx.protocol.bedrock.data.PlayerActionType;
 import com.nukkitx.protocol.bedrock.data.PlayerBlockActionData;
 import com.nukkitx.protocol.bedrock.v428.serializer.PlayerAuthInputSerializer_v428;
 import io.netty.buffer.ByteBuf;
@@ -17,20 +15,12 @@ public class PlayerAuthInputSerializer_v431 extends PlayerAuthInputSerializer_v4
     @Override
     protected void writePlayerBlockActionData(ByteBuf buffer, BedrockPacketHelper helper, PlayerBlockActionData actionData) {
         super.writePlayerBlockActionData(buffer, helper, actionData);
-        if (actionData.getAction() == PlayerActionType.STOP_BREAK) {
-            helper.writeVector3i(buffer, actionData.getBlockPosition());
-            VarInts.writeInt(buffer, actionData.getFace());
-        }
+        // PlayerActionType.STOP_BREAK - NOOP
     }
 
     @Override
     protected PlayerBlockActionData readPlayerBlockActionData(ByteBuf buffer, BedrockPacketHelper helper) {
-        PlayerBlockActionData data = super.readPlayerBlockActionData(buffer, helper);
-
-        if (data.getAction() == PlayerActionType.STOP_BREAK) {
-            data.setBlockPosition(helper.readVector3i(buffer));
-            data.setFace(VarInts.readInt(buffer));
-        }
-        return data;
+        // PlayerActionType.STOP_BREAK - NOOP
+        return super.readPlayerBlockActionData(buffer, helper);
     }
 }
