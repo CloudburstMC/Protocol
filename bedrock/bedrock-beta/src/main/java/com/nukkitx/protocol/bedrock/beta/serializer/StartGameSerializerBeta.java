49323e07716a7c382a65ad007d4263db4ddcfdf8
Niek Vincent <apocalypsjenl@gmail.com>
2022-05-14 19:10:02 +0200

    Fixed the correct order of the StartGamePacket values

--- bedrock/bedrock-beta/src/main/java/com/nukkitx/protocol/bedrock/beta/serializer/StartGameSerializerBeta.java
+++ bedrock/bedrock-beta/src/main/java/com/nukkitx/protocol/bedrock/beta/serializer/StartGameSerializerBeta.java
@@ -1,31 +1,42 @@
 package com.nukkitx.protocol.bedrock.beta.serializer;
 
-import com.nukkitx.nbt.NbtType;
 import com.nukkitx.protocol.bedrock.BedrockPacketHelper;
 import com.nukkitx.protocol.bedrock.BedrockSession;
 import com.nukkitx.protocol.bedrock.packet.StartGamePacket;
-import com.nukkitx.protocol.bedrock.v503.serializer.StartGameSerializer_v503;
+import com.nukkitx.protocol.bedrock.v465.serializer.StartGameSerializer_v465;
 import io.netty.buffer.ByteBuf;
 import lombok.AccessLevel;
 import lombok.NoArgsConstructor;
 
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
-public class StartGameSerializerBeta extends StartGameSerializer_v503 {
+public class StartGameSerializerBeta extends StartGameSerializer_v465 {
     public static final StartGameSerializerBeta INSTANCE = new StartGameSerializerBeta();
 
     @Override
-    public void serialize(ByteBuf buffer, BedrockPacketHelper helper, StartGamePacket packet) {
-        super.serialize(buffer, helper, packet);
+    public void serialize(ByteBuf buffer, BedrockPacketHelper helper, StartGamePacket packet, BedrockSession session) {
+        super.serialize(buffer, helper, packet, session);
 
         helper.writeTag(buffer, packet.getPlayerPropertyData());
+        buffer.writeLongLE(packet.getBlockRegistryChecksum());
         helper.writeUuid(buffer, packet.getWorldTemplateId());
     }
 
     @Override
     public void deserialize(ByteBuf buffer, BedrockPacketHelper helper, StartGamePacket packet, BedrockSession session) {
-        super.deserialize(buffer, helper, packet);
+        super.deserialize(buffer, helper, packet, session);
 
         packet.setPlayerPropertyData(helper.readTag(buffer));
+        packet.setBlockRegistryChecksum(buffer.readLongLE());
         packet.setWorldTemplateId(helper.readUuid(buffer));
+    }
+
+    @Override
+    protected long readSeed(ByteBuf buf) {
+        return buf.readLongLE();
+    }
+
+    @Override
+    protected void writeSeed(ByteBuf buf, long seed) {
+        buf.writeLongLE(seed);
     }
 }
