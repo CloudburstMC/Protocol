e41ad67d13a0c40528d37d1d7f10b98b8468ce70
RednedEpic <redned235@gmail.com>
2022-10-30 10:44:08 -0500

    Fix compile

--- common/src/main/java/org/cloudburstmc/protocol/common/PlayerSession.java
+++ common/src/main/java/org/cloudburstmc/protocol/common/PlayerSession.java
@@ -1,7 +1,5 @@
 package org.cloudburstmc.protocol.common;
 
-import com.nukkitx.network.util.DisconnectReason;
-
 import javax.annotation.Nonnull;
 
 public interface PlayerSession {
@@ -9,8 +7,6 @@ public interface PlayerSession {
     boolean isClosed();
 
     void close();
-
-    void onDisconnect(@Nonnull DisconnectReason reason);
 
     void onDisconnect(@Nonnull String reason);
 }
