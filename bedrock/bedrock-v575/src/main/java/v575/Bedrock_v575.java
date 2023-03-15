5aa2e1f61beb3cf09d824575b9d9347e01e8ec81
CoolLoong <1542536763@qq.com>
2023-03-15 23:34:18 +0800

    add PlayerAuthInputSerializer

--- bedrock/bedrock-v575/src/main/java/v575/Bedrock_v575.java
+++ bedrock/bedrock-v575/src/main/java/v575/Bedrock_v575.java
@@ -4,11 +4,13 @@ package v575;
 import com.nukkitx.protocol.bedrock.BedrockPacketCodec;
 import com.nukkitx.protocol.bedrock.packet.CameraInstructionPacket;
 import com.nukkitx.protocol.bedrock.packet.CameraPresetsPacket;
+import com.nukkitx.protocol.bedrock.packet.PlayerAuthInputPacket;
 import com.nukkitx.protocol.bedrock.packet.UnlockedRecipesPacket;
 import com.nukkitx.protocol.bedrock.v567.Bedrock_v567patch;
 import lombok.experimental.UtilityClass;
 import v575.serializer.CameraInstructionSerializer_v575;
 import v575.serializer.CameraPresetsSerializer_v575;
+import v575.serializer.PlayerAuthInputSerializer_v575;
 import v575.serializer.UnlockedRecipesSerializer_v575;
 
 @UtilityClass
@@ -19,6 +21,7 @@ public class Bedrock_v575 {
             .protocolVersion(575)
             .minecraftVersion("1.19.70")
             .helper(BedrockPacketHelper_v575.INSTANCE)
+            .registerPacket(PlayerAuthInputPacket.class, new PlayerAuthInputSerializer_v575(), 144)
             .registerPacket(CameraPresetsPacket.class, new CameraPresetsSerializer_v575(), 198)
             .registerPacket(UnlockedRecipesPacket.class, new UnlockedRecipesSerializer_v575(), 199)
             .registerPacket(CameraInstructionPacket.class, new CameraInstructionSerializer_v575(), 300)
