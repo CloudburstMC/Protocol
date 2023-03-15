55b01ec5ecf57732632ccac96ebb11b02519528c
CoolLoong <1542536763@qq.com>
2023-03-15 23:29:13 +0800

    extend Bedrock_v567patch packet

--- bedrock/bedrock-v575/src/main/java/v575/Bedrock_v575.java
+++ bedrock/bedrock-v575/src/main/java/v575/Bedrock_v575.java
@@ -5,6 +5,7 @@ import com.nukkitx.protocol.bedrock.BedrockPacketCodec;
 import com.nukkitx.protocol.bedrock.packet.CameraInstructionPacket;
 import com.nukkitx.protocol.bedrock.packet.CameraPresetsPacket;
 import com.nukkitx.protocol.bedrock.packet.UnlockedRecipesPacket;
+import com.nukkitx.protocol.bedrock.v567.Bedrock_v567patch;
 import lombok.experimental.UtilityClass;
 import v575.serializer.CameraInstructionSerializer_v575;
 import v575.serializer.CameraPresetsSerializer_v575;
@@ -13,7 +14,7 @@ import v575.serializer.UnlockedRecipesSerializer_v575;
 @UtilityClass
 public class Bedrock_v575 {
 
-    public static final BedrockPacketCodec V575_CODEC = BedrockPacketCodec.builder()
+    public static final BedrockPacketCodec V575_CODEC = Bedrock_v567patch.BEDROCK_V567PATCH.toBuilder()
             .raknetProtocolVersion(11)
             .protocolVersion(575)
             .minecraftVersion("1.19.70")
