b9b78f51acda2eeb3b45f112eb2258b29cddc64c
Camotoy <20743703+Camotoy@users.noreply.github.com>
2023-04-08 14:40:32 -0400

    Fix encoding of UpdateSoftEnumPacket

--- bedrock-codec/src/main/java/org/cloudburstmc/protocol/bedrock/data/command/CommandEnumData.java
+++ bedrock-codec/src/main/java/org/cloudburstmc/protocol/bedrock/data/command/CommandEnumData.java
@@ -2,12 +2,12 @@ package org.cloudburstmc.protocol.bedrock.data.command;
 
 import lombok.Value;
 
-import java.util.LinkedHashMap;
+import java.util.Map;
 import java.util.Set;
 
 @Value
 public class CommandEnumData {
     String name;
-    LinkedHashMap<String, Set<CommandEnumConstraint>> values;
+    Map<String, Set<CommandEnumConstraint>> values;
     boolean isSoft;
 }
