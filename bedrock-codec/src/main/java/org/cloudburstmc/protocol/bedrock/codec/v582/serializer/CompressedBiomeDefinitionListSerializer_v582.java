06b207eb63e63406ba2e1aebb71bbc057464cff3
Alemiz <alex@mizerak.eu>
2023-04-23 21:44:57 +0200

    Compressed NBT is prefixed with it's length

--- bedrock-codec/src/main/java/org/cloudburstmc/protocol/bedrock/codec/v582/serializer/CompressedBiomeDefinitionListSerializer_v582.java
+++ bedrock-codec/src/main/java/org/cloudburstmc/protocol/bedrock/codec/v582/serializer/CompressedBiomeDefinitionListSerializer_v582.java
@@ -7,20 +7,26 @@ import org.cloudburstmc.nbt.NbtMap;
 import org.cloudburstmc.protocol.bedrock.codec.BedrockCodecHelper;
 import org.cloudburstmc.protocol.bedrock.codec.BedrockPacketSerializer;
 import org.cloudburstmc.protocol.bedrock.packet.CompressedBiomeDefinitionListPacket;
+import org.cloudburstmc.protocol.common.util.VarInts;
 
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class CompressedBiomeDefinitionListSerializer_v582 implements BedrockPacketSerializer<CompressedBiomeDefinitionListPacket> {
     public static final CompressedBiomeDefinitionListSerializer_v582 INSTANCE = new CompressedBiomeDefinitionListSerializer_v582();
-    protected static final byte[] COMPRESSED_INDICATOR = new byte[]{ (byte) 0xe4, (byte) 0x92, 0x3f, 0x43, 0x4f, 0x4d, 0x50, 0x52, 0x45, 0x53, 0x53, 0x45, 0x44 }; // __?COMPRESSED
+    protected static final byte[] COMPRESSED_INDICATOR = new byte[]{ 0x43, 0x4f, 0x4d, 0x50, 0x52, 0x45, 0x53, 0x53, 0x45, 0x44 }; // COMPRESSED
 
     @Override
     public void serialize(ByteBuf buffer, BedrockCodecHelper helper, CompressedBiomeDefinitionListPacket packet) {
-        this.writeCompressed(packet.getDefinitions(), buffer, helper);
+        ByteBuf compressed = buffer.alloc().ioBuffer();
+        this.writeCompressed(packet.getDefinitions(), compressed, helper);
+
+        VarInts.writeUnsignedInt(buffer, compressed.readableBytes());
+        buffer.writeBytes(compressed);
     }
 
     @Override
     public void deserialize(ByteBuf buffer, BedrockCodecHelper helper, CompressedBiomeDefinitionListPacket packet) {
-        packet.setDefinitions(this.readCompressed(buffer, helper, COMPRESSED_INDICATOR.length));
+        int length = VarInts.readUnsignedInt(buffer);
+        packet.setDefinitions(this.readCompressed(buffer.readBytes(length), helper, COMPRESSED_INDICATOR.length));
     }
 
     protected NbtMap readCompressed(ByteBuf buffer, BedrockCodecHelper helper, int length) {
