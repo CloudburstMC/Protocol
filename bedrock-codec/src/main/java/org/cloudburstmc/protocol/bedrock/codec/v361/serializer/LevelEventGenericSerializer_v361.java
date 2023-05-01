77f941e2702a011ecdec6ac51a084b9f601e31b4
davchoo <4722249+davchoo@users.noreply.github.com>
2023-05-01 03:10:30 -0400

    Fix LevelEventGenericSerializer
    Based on https://github.com/CloudburstMC/Protocol/commit/86c5d03740ba5b1a71745272bc545cc737e37075

--- bedrock-codec/src/main/java/org/cloudburstmc/protocol/bedrock/codec/v361/serializer/LevelEventGenericSerializer_v361.java
+++ bedrock-codec/src/main/java/org/cloudburstmc/protocol/bedrock/codec/v361/serializer/LevelEventGenericSerializer_v361.java
@@ -2,6 +2,7 @@ package org.cloudburstmc.protocol.bedrock.codec.v361.serializer;
 
 import io.netty.buffer.ByteBuf;
 import lombok.RequiredArgsConstructor;
+import org.cloudburstmc.nbt.NbtType;
 import org.cloudburstmc.protocol.bedrock.codec.BedrockCodecHelper;
 import org.cloudburstmc.protocol.bedrock.codec.BedrockPacketSerializer;
 import org.cloudburstmc.protocol.bedrock.data.LevelEventType;
@@ -17,13 +18,13 @@ public class LevelEventGenericSerializer_v361 implements BedrockPacketSerializer
     @Override
     public void serialize(ByteBuf buffer, BedrockCodecHelper helper, LevelEventGenericPacket packet) {
         VarInts.writeInt(buffer, typeMap.getId(packet.getType()));
-        helper.writeTag(buffer, packet.getTag());
+        helper.writeTagValue(buffer, packet.getTag());
     }
 
     @Override
     public void deserialize(ByteBuf buffer, BedrockCodecHelper helper, LevelEventGenericPacket packet) {
         int eventId = VarInts.readInt(buffer);
         packet.setType(typeMap.getType(eventId));
-        packet.setTag(helper.readTag(buffer, Object.class));
+        packet.setTag(helper.readTagValue(buffer, NbtType.COMPOUND));
     }
 }
