a51c3cd32ae9dfbab3feb5d024dfd58044a8f405
Alemiz <alex@mizerak.eu>
2024-02-26 21:14:47 +0100

    This does not use VarInts

--- bedrock-codec/src/main/java/org/cloudburstmc/protocol/bedrock/codec/v662/serializer/MobEffectSerializer_v662.java
+++ bedrock-codec/src/main/java/org/cloudburstmc/protocol/bedrock/codec/v662/serializer/MobEffectSerializer_v662.java
@@ -4,7 +4,6 @@ import io.netty.buffer.ByteBuf;
 import org.cloudburstmc.protocol.bedrock.codec.BedrockCodecHelper;
 import org.cloudburstmc.protocol.bedrock.codec.v291.serializer.MobEffectSerializer_v291;
 import org.cloudburstmc.protocol.bedrock.packet.MobEffectPacket;
-import org.cloudburstmc.protocol.common.util.VarInts;
 
 public class MobEffectSerializer_v662 extends MobEffectSerializer_v291 {
     public static final MobEffectSerializer_v662 INSTANCE = new MobEffectSerializer_v662();
@@ -12,12 +11,12 @@ public class MobEffectSerializer_v662 extends MobEffectSerializer_v291 {
     @Override
     public void serialize(ByteBuf buffer, BedrockCodecHelper helper, MobEffectPacket packet) {
         super.serialize(buffer, helper, packet);
-        VarInts.writeUnsignedLong(buffer, packet.getTick());
+        buffer.writeLongLE(packet.getTick());
     }
 
     @Override
     public void deserialize(ByteBuf buffer, BedrockCodecHelper helper, MobEffectPacket packet) {
         super.deserialize(buffer, helper, packet);
-        packet.setTick(VarInts.readUnsignedLong(buffer));
+        packet.setTick(buffer.readLongLE());
     }
 }
