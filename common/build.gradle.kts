92c9c0ed5afe0020974a91a41f416dff6281ea64
SupremeMortal <6178101+SupremeMortal@users.noreply.github.com>
2024-06-16 14:10:59 +0100

    Update to Fastutil release
    Update from `8.5.13-SNAPSHOT` to `8.5.13` release, and enforce BOM to ensure versioning is consistent.

--- common/build.gradle.kts
+++ common/build.gradle.kts
@@ -1,5 +1,6 @@
 dependencies {
     api(libs.netty.buffer)
+    api(enforcedPlatform(libs.fastutil.bom))
     api(libs.fastutil.int.`object`.maps)
     api(libs.fastutil.`object`.int.maps)
     api(libs.math)
