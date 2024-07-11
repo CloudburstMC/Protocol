CloudburstMC/Protocol

commit 1a6cbdf864c24ad6dc3a8f6ac8562d7b472f8b92

Implement EncodingSettings.UNLIMITED

bedrock-codec/src/main/java/org/cloudburstmc/protocol/bedrock/data/EncodingSettings.java
@@ -48,6 +48,20 @@ public class EncodingSettings {
             .maxStringLength(1024 * 32) // 32KB
             .build();
 
+    /**
+     * A {@link EncodingSettings} instance for implementations
+     * that don't need such limits. (e.g. Proxy server client <-> downstream server connection)
+     * This setting is not generally recommended for use in most cases,
+     * as it will allow any large packets to be received.
+     */
+    public static final EncodingSettings UNLIMITED = EncodingSettings.builder()
+            .maxListSize(-1)
+            .maxByteArraySize(-1)
+            .maxNetworkNBTSize(-1)
+            .maxItemNBTSize(-1)
+            .maxStringLength(-1)
+            .build();
+
     private final int maxListSize;
     private final int maxByteArraySize;
     private final int maxNetworkNBTSize;

commit d005a4c74ecde92dfda85a8a42ede1d209c2567f

Fix deploy caused by 1a6cbdf

bedrock-codec/src/main/java/org/cloudburstmc/protocol/bedrock/data/EncodingSettings.java
@@ -50,7 +50,7 @@ public class EncodingSettings {
 
     /**
      * A {@link EncodingSettings} instance for implementations
-     * that don't need such limits. (e.g. Proxy server client <-> downstream server connection)
+     * that don't need such limits. (e.g. connection between Proxy server client and downstream server)
      * This setting is not generally recommended for use in most cases,
      * as it will allow any large packets to be received.
      */