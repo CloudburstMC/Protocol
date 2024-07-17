6588e324666f61909c20ddf112f16314c19d0767
Kaooot <37877491+Kaooot@users.noreply.github.com>
2024-07-17 12:21:23 +0200

    Update Bedrock_v705

--- bedrock-codec/src/main/java/org/cloudburstmc/protocol/bedrock/codec/v705/Bedrock_v705.java
+++ bedrock-codec/src/main/java/org/cloudburstmc/protocol/bedrock/codec/v705/Bedrock_v705.java
@@ -4,7 +4,7 @@ import org.cloudburstmc.protocol.bedrock.codec.BedrockCodec;
 import org.cloudburstmc.protocol.bedrock.codec.v291.serializer.LevelSoundEvent1Serializer_v291;
 import org.cloudburstmc.protocol.bedrock.codec.v313.serializer.LevelSoundEvent2Serializer_v313;
 import org.cloudburstmc.protocol.bedrock.codec.v332.serializer.LevelSoundEventSerializer_v332;
-import org.cloudburstmc.protocol.bedrock.codec.v685.Bedrock_v685;
+import org.cloudburstmc.protocol.bedrock.codec.v686.Bedrock_v686;
 import org.cloudburstmc.protocol.bedrock.codec.v705.serializer.*;
 import org.cloudburstmc.protocol.bedrock.data.PacketRecipient;
 import org.cloudburstmc.protocol.bedrock.data.SoundEvent;
@@ -12,22 +12,22 @@ import org.cloudburstmc.protocol.bedrock.data.inventory.itemstack.request.action
 import org.cloudburstmc.protocol.bedrock.packet.*;
 import org.cloudburstmc.protocol.common.util.TypeMap;
 
-public class Bedrock_v705 extends Bedrock_v685 {
+public class Bedrock_v705 extends Bedrock_v686 {
 
-    protected static final TypeMap<SoundEvent> SOUND_EVENTS = Bedrock_v685.SOUND_EVENTS
+    protected static final TypeMap<SoundEvent> SOUND_EVENTS = Bedrock_v686.SOUND_EVENTS
             .toBuilder()
             .insert(510, SoundEvent.IMITATE_BOGGED)
             .replace(530, SoundEvent.VAULT_REJECT_REWARDED_PLAYER)
             .insert(531, SoundEvent.UNDEFINED)
             .build();
 
-    protected static final TypeMap<ItemStackRequestActionType> ITEM_STACK_REQUEST_TYPES = Bedrock_v685.ITEM_STACK_REQUEST_TYPES
+    protected static final TypeMap<ItemStackRequestActionType> ITEM_STACK_REQUEST_TYPES = Bedrock_v686.ITEM_STACK_REQUEST_TYPES
             .toBuilder()
             .remove(7)
             .remove(8)
             .build();
 
-    public static final BedrockCodec CODEC = Bedrock_v685.CODEC.toBuilder()
+    public static final BedrockCodec CODEC = Bedrock_v686.CODEC.toBuilder()
             .raknetProtocolVersion(11)
             .protocolVersion(705)
             .minecraftVersion("1.21.10")
@@ -44,7 +44,6 @@ public class Bedrock_v705 extends Bedrock_v685 {
             .updateSerializer(PlayerAuthInputPacket.class, PlayerAuthInputSerializer_v705.INSTANCE)
             .updateSerializer(SetTitlePacket.class, SetTitleSerializer_v705.INSTANCE)
             .updateSerializer(StopSoundPacket.class, StopSoundSerializer_v705.INSTANCE)
-            .registerPacket(ClientboundCloseFormPacket::new, ClientboundCloseFormSerializer_v705.INSTANCE, 310, PacketRecipient.CLIENT)
             .registerPacket(ServerboundLoadingScreenPacket::new, ServerboundLoadingScreenSerializer_v705.INSTANCE, 312, PacketRecipient.SERVER)
             .registerPacket(JigsawStructureDataPacket::new, JigsawStructureDataSerializer_v705.INSTANCE, 313, PacketRecipient.CLIENT)
             .build();
