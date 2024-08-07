02581c877dd774ea29f2097b70b12951ba0acf79
Alemiz <alex@mizerak.eu>
2024-08-07 16:54:26 +0200

    Backward compatibility for compat DisconnectPacket

--- bedrock-codec/src/main/java/org/cloudburstmc/protocol/bedrock/codec/compat/serializer/DisconnectSerializerCompat.java
+++ bedrock-codec/src/main/java/org/cloudburstmc/protocol/bedrock/codec/compat/serializer/DisconnectSerializerCompat.java
@@ -20,6 +20,7 @@ public class DisconnectSerializerCompat implements BedrockPacketSerializer<Disco
         buffer.writeBoolean(packet.isMessageSkipped());
         if (!packet.isMessageSkipped()) {
             helper.writeString(buffer, packet.getKickMessage());
+            helper.writeString(buffer, packet.getFilteredMessage());
         }
     }
 
@@ -31,6 +32,9 @@ public class DisconnectSerializerCompat implements BedrockPacketSerializer<Disco
         packet.setMessageSkipped(buffer.readBoolean());
         if (!packet.isMessageSkipped()) {
             packet.setKickMessage(helper.readString(buffer));
+            if (buffer.isReadable()) { // backwards compatibility
+                packet.setFilteredMessage(helper.readString(buffer));
+            }
         }
     }
 }
