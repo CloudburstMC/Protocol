43af940c609309e9fb4117ab36ef237baf0aab1e
Kaooot <37877491+Kaooot@users.noreply.github.com>
2024-08-14 14:49:35 +0200

    Fix ResourcePacksInfoSerializer_v712

--- bedrock-codec/src/main/java/org/cloudburstmc/protocol/bedrock/codec/v712/serializer/ResourcePacksInfoSerializer_v712.java
+++ bedrock-codec/src/main/java/org/cloudburstmc/protocol/bedrock/codec/v712/serializer/ResourcePacksInfoSerializer_v712.java
@@ -5,13 +5,26 @@ import org.cloudburstmc.protocol.bedrock.codec.BedrockCodecHelper;
 import org.cloudburstmc.protocol.bedrock.codec.v662.serializer.ResourcePacksInfoSerializer_v622;
 import org.cloudburstmc.protocol.bedrock.packet.ResourcePacksInfoPacket;
 
+import java.util.Objects;
+
 public class ResourcePacksInfoSerializer_v712 extends ResourcePacksInfoSerializer_v622 {
     public static final ResourcePacksInfoSerializer_v712 INSTANCE = new ResourcePacksInfoSerializer_v712();
 
     @Override
     public void writeEntry(ByteBuf buffer, BedrockCodecHelper helper, ResourcePacksInfoPacket.Entry entry, boolean resource) {
-        super.writeEntry(buffer, helper, entry, resource);
+        Objects.requireNonNull(entry, "ResourcePacketInfoPacket entry was null");
+
+        helper.writeString(buffer, entry.getPackId());
+        helper.writeString(buffer, entry.getPackVersion());
+        buffer.writeLongLE(entry.getPackSize());
+        helper.writeString(buffer, entry.getContentKey());
+        helper.writeString(buffer, entry.getSubPackName());
+        helper.writeString(buffer, entry.getContentId());
+        buffer.writeBoolean(entry.isScripting());
         buffer.writeBoolean(entry.isAddonPack());
+        if (resource) {
+            buffer.writeBoolean(entry.isRaytracingCapable());
+        }
     }
 
     @Override
