b654e96dbce04d55ae45844a7431d7461c9baefb
Kevims <kcodeyt@gmail.com>
2024-08-16 17:18:56 +0200

    Fix maven publish error

--- common/build.gradle.kts
+++ common/build.gradle.kts
@@ -12,3 +12,8 @@ tasks.jar {
         attributes("Automatic-Module-Name" to "org.cloudburstmc.protocol.common")
     }
 }
+
+// https://docs.gradle.org/8.0.1/userguide/publishing_setup.html#sec:suppressing_validation_errors
+tasks.withType<GenerateModuleMetadata> {
+    suppressedValidationErrors.add("enforced-platform")
+}
