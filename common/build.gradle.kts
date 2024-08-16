a9e7ca6e82fd7c2e382673bc09c99fddd609d622
Kevims <kcodeyt@gmail.com>
2024-08-16 17:36:28 +0200

    Use platform instead of enforcedPlatform

--- common/build.gradle.kts
+++ common/build.gradle.kts
@@ -1,6 +1,6 @@
 dependencies {
     api(libs.netty.buffer)
-    api(enforcedPlatform(libs.fastutil.bom))
+    api(platform(libs.fastutil.bom))
     api(libs.fastutil.int.`object`.maps)
     api(libs.fastutil.`object`.int.maps)
     api(libs.math)
@@ -11,9 +11,4 @@ tasks.jar {
     manifest {
         attributes("Automatic-Module-Name" to "org.cloudburstmc.protocol.common")
     }
-}
-
-// https://docs.gradle.org/8.0.1/userguide/publishing_setup.html#sec:suppressing_validation_errors
-tasks.withType<GenerateModuleMetadata> {
-    suppressedValidationErrors.add("enforced-platform")
 }
