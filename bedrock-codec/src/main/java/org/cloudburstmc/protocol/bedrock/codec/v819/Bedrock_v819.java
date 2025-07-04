3bb1bdd186fea6db6064ca70898e415b13028941
daoge_cmd <3523206925@qq.com>
2025-07-04 20:43:42 +0800

    feat: update SOUND_EVENTS type map

--- bedrock-codec/src/main/java/org/cloudburstmc/protocol/bedrock/codec/v819/Bedrock_v819.java
+++ bedrock-codec/src/main/java/org/cloudburstmc/protocol/bedrock/codec/v819/Bedrock_v819.java
@@ -1,16 +1,27 @@
 package org.cloudburstmc.protocol.bedrock.codec.v819;
 
 import org.cloudburstmc.protocol.bedrock.codec.BedrockCodec;
+import org.cloudburstmc.protocol.bedrock.codec.v786.serializer.LevelSoundEventSerializer_v786;
 import org.cloudburstmc.protocol.bedrock.codec.v818.Bedrock_v818;
+import org.cloudburstmc.protocol.bedrock.data.SoundEvent;
+import org.cloudburstmc.protocol.bedrock.packet.LevelSoundEventPacket;
+import org.cloudburstmc.protocol.common.util.TypeMap;
 
 /**
  * @author daoge_cmd
  */
 public class Bedrock_v819 extends Bedrock_v818 {
 
+    protected static final TypeMap<SoundEvent> SOUND_EVENTS = Bedrock_v818.SOUND_EVENTS
+            .toBuilder()
+            .replace(561, SoundEvent.RECORD_LAVA_CHICKEN)
+            .insert(562, SoundEvent.UNDEFINED)
+            .build();
+
     public static final BedrockCodec CODEC = Bedrock_v818.CODEC.toBuilder()
             .minecraftVersion("1.21.93")
             .protocolVersion(819)
+            .updateSerializer(LevelSoundEventPacket.class, new LevelSoundEventSerializer_v786(SOUND_EVENTS))
             .build();
 
 }
