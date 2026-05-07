8188979deaf91b4c46e3f60ce2d95be420576f7d
SleepyPop <81194303+SleepyPopp@users.noreply.github.com>
2026-05-07 08:20:02 -0400

    Revert changes in DimensionDataSerializer_v503.java

--- bedrock-codec/src/main/java/org/cloudburstmc/protocol/bedrock/codec/v503/serializer/DimensionDataSerializer_v503.java
+++ bedrock-codec/src/main/java/org/cloudburstmc/protocol/bedrock/codec/v503/serializer/DimensionDataSerializer_v503.java
@@ -31,7 +31,6 @@ public class DimensionDataSerializer_v503 implements BedrockPacketSerializer<Dim
         int maximumHeight = VarInts.readInt(buffer);
         int minimumHeight = VarInts.readInt(buffer);
         int generatorType = VarInts.readInt(buffer);
-        int dimensionType = VarInts.readInt(buffer);
-        return new DimensionDefinition(id, maximumHeight, minimumHeight, generatorType, dimensionType);
+        return new DimensionDefinition(id, maximumHeight, minimumHeight, generatorType);
     }
 }
